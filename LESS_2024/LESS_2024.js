08089c3aadb93f68b302d31672e75cbb1b7da549
karen ann <karenanndonnachie@gmail.com>
2024-09-14 00:23:00 +1000

    Update LESS_2024.js

--- LESS_2024/LESS_2024.js
+++ LESS_2024/LESS_2024.js
@@ -1,4 +1,4 @@
-var pic, pic2, pic3, pic4, pic5, pic6, pic7, pics;
+var pic, pic2, pic3, pic4, pic5, pic6, pic7, pics, randomHeight;
 var bgcol="blue";
 var fillcol="red";
 var bgblue=true;
@@ -19,37 +19,44 @@ function setup() {
   background(bgcol);
   fill(fillcol);
   imageMode(CENTER, CENTER);
+  //rectMode(CENTER, CENTER);
   textFont(font);
   textAlign(LEFT, CENTER);
   textSize(width/5);
-  textLeading(width/12);
-   textWrap(CHAR);
-  text("COLLIDEDLESSFESTIVALOFCONTEMPORARYCOLLAGE2024SKOVGAARDMUSEET",0,60,width,height);
+  textLeading(height/13);
+  //textLeading(width/24);
+  textWrap(CHAR);
+  text("COLLIDEDLESSFESTIVALOFCONTEMPORARYCOLLAGE2024SKOVGAARDMUSEET", 0, 60,0.9*width,0.9*height);
   pics=[pic, pic2, pic3, pic4, pic5, pic6, pic7];
-
+  randomHeight=random(10,100);
+  
 }
 
 function draw() {
   
-  if (frameCount>200){
+  if (frameCount>120){
   //let g = myRadio.value();
-  background(bgcol); 
+  //background(bgcol); 
+  if (bgblue===true){background(0, 0, 255, 7);}
+  else{background(255, 0, 0, 7);}
   let thispic = int(map(frameCount, 200, 800, 0, 6));
-  image(pics[thispic], width/2, height/2);
+  if (frameCount%100==0){randomHeight=random(10,100);}
+  ar = pics[thispic].width / pics[thispic].height;
+  image(pics[thispic], randomHeight/100*width, randomHeight/100*height, ar*height, height);
 
   textSize(width/5);
   fill(fillcol);
-  textLeading(height/48 * width/ (mouseX - mouseY));
+  textLeading(height/48 * width/ (mouseY - mouseX));
   noStroke();
   select('canvas').elt.style.letterSpacing = "1px";
   textWrap(CHAR);
-  scale(map(mouseX, 0, width, 0.5, 1.5));
-  text("COLLIDEDLESSFESTIVALOFCONTEMPORARYCOLLAGE2024SKOVGAARDMUSEET",0,60,width,height);
+  //scale(map(mouseX, 0, width, 1, 1.5), map(mouseY, 0, height, 1, 4));
+  text("COLLIDEDLESSFESTIVALOFCONTEMPORARYCOLLAGE2024SKOVGAARDMUSEET",0,0,width,height);
   //background("blue");
   //image (pic, width/2, height/2, height/pic.height*pic.width, height);
   //image(pic, mouseX, mouseY, 100, 100);
   }
-  if (frameCount>800){frameCount=200;  textLeading(width/12); scale(1);
+  if (frameCount>800){frameCount=100; background(bgcol);fill(fillcol); textLeading(width/12); scale(1);randomHeight=random(10,100);
 }
 }
 function mousePressed(){
